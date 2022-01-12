bcffcc629d6c9cbaeebd14eb860426bd00ca4de9
mojeee <mojtaba.amini.1995@gmail.com>
2022-01-13 00:53:00 +0100

    The accuracy withSVN was higher: ACC: 0.80140

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ Today is December 21
 Machine Learning Project for Course project of Machine Learning and Deep Learning
 This is my first try to do a complete python project as a Data Science Student 
 """
+import inline as inline
+import matplotlib
 import pandas as pd
 import re
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+import seaborn as sns
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS , ImageColorGenerator
+from sklearn import svm
+
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn import feature_extraction, linear_model, model_selection, preprocessing
@@ -26,6 +33,10 @@ def cleanend_data(text):
     text = re.sub('[\#]', '', text)
     text = re.sub('[\'\"]', '', text)
     text = re.sub('[\.\$\@\!\*\&\?\_\__]', '', text)
+    '''stop_words =["'tis", "tis", "a", "amp", "30th", "22", "ye", "I", "you", "20th", "me", "get", "st",
+                    "sráid", "ll", "le", "tú", "bhí", "faithfully", "tír","https","lol","im"]
+    for a in stop_words:
+        text=text.replace(a, "")'''
     return text
 
 if __name__ == '__main__':
@@ -35,12 +46,33 @@ if __name__ == '__main__':
     raw_data = pd.read_csv(path)
     #print(raw_data.head(10))
     raw_data['text_clean'] = raw_data.text.map(cleanend_data)
-    stop_words =frozenset(["'tis", "tis", "a", "amp", "30th", "22", "ye", "I", "you", "20th", "me", "get", "st",
-                    "sráid", "ll", "le", "tú", "bhí", "faithfully", "tír"])
+
     del raw_data['text']
     del raw_data['id']
     del raw_data['location']
     del raw_data['keyword']
+    sns.countplot(x='target', data=raw_data)
+    plt.show()
+    #**** word cloud
+    #df = pd.DataFrame(raw_data[['text_clean', 'Tweet']])
+
+    Df_critical = raw_data[raw_data['target'] == 1]
+    Df_non_critical= raw_data[raw_data['target'] == 0]
+
+    all_tweet = " ".join(review for review in raw_data.text_clean)
+
+    #mask = np.array(Image.open("./nlp-getting-started/Italy.png"))
+    wordcloud_ALL = WordCloud(max_font_size=50, max_words=1000, background_color="white").generate(all_tweet)
+    plt.imshow(wordcloud_ALL, interpolation='bilinear')
+    # create coloring from image
+    #image_colors = ImageColorGenerator(mask)
+    #plt.figure(figsize=[7, 7])
+    #plt.imshow(wordcloud_ita.recolor(color_func=image_colors), interpolation="bilinear")
+    plt.axis("off")
+#*************************
+    # store to file
+    #plt.savefig("img/ita_wine.png", format="png")
+    plt.show()
     y_data=raw_data['target'].copy()
     del raw_data['target']
     X_train, X_val, y_train, y_val = train_test_split(raw_data, y_data, train_size=0.75, random_state=123)
@@ -59,6 +91,25 @@ if __name__ == '__main__':
     tr_acc = accuracy_score(y_train, y_train_pred_lr)
     val_acc = accuracy_score(y_val, y_val_pred_lr)
     print("the accuracy score of the train is : {}, and the accuracy score of validation is : {}".format(tr_acc,val_acc))
+    C = [0.001, 0.01, 0.1, 1.,1.5,2, 10, 100]
+    for c in C:
+        classmodel = LogisticRegression(C=c,max_iter = 200)
+        classmodel.fit(X_tr_vec, y_train)
+        y_train_pred_lr = classmodel.predict(X_tr_vec)
+        y_val_pred_lr = classmodel.predict(X_val_vec)
+        tr_acc = accuracy_score(y_train, y_train_pred_lr)
+        val_acc = accuracy_score(y_val, y_val_pred_lr)
+
+        print(f"LR. C= {c}.\tTrain ACC: {tr_acc}\tVal Acc: {val_acc}")
+    #*************************************** SVM Method *************************************************
+    clf = svm.SVC(kernel='rbf')
+    clf.fit(X_tr_vec, y_train)
+    y_train_pred_lr = clf.predict(X_tr_vec)
+    y_val_pred_lr = clf.predict(X_val_vec)
+    tr_acc = accuracy_score(y_train, y_train_pred_lr)
+    val_acc = accuracy_score(y_val, y_val_pred_lr)
+    print(f"SVM method \tTrain ACC: {tr_acc}\tVal Acc: {val_acc}")
+
     #************************************************** train data ******************************************************************
     path = "./nlp-getting-started/test.csv"
     test_data = pd.read_csv(path)
@@ -76,7 +127,7 @@ if __name__ == '__main__':
     test_data = cv.transform(test_data.text_clean)
     column_name = cv.get_feature_names_out()
     X_test_vec=pd.DataFrame(test_data.toarray(),columns=column_name)
-    out_data["target"] = classmodel.predict(X_test_vec)
+    out_data["target"] = clf.predict(X_test_vec)
     #test_acc = accuracy_score(y_test_data, y_test_pred_lr)
     #print("the accuracy score of the test is : {}".format(test_acc))
     print(out_data.head(10))
