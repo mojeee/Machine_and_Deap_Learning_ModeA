9a2158c7b4c4c73e2ce76fa0434583d1097d0838
mojeee <mojtaba.amini.1995@gmail.com>
2022-01-13 13:31:03 +0100

    The accuracy with SVM and adding some stop words was higher than before: ACC: 0.80753

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ This is my first try to do a complete python project as a Data Science Student
 """
 import inline as inline
 import matplotlib
+import nltk
 import pandas as pd
 import re
 from sklearn.feature_extraction.text import CountVectorizer
@@ -19,6 +20,11 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS , ImageColorGenerator
 from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+from nltk.tokenize import word_tokenize
+
 
 import numpy as np
 from sklearn.preprocessing import StandardScaler
@@ -27,12 +33,24 @@ from sklearn import feature_extraction, linear_model, model_selection, preproces
 def cleanend_data(text):
     # Use a breakpoint in the code line below to debug your script.
     text = text.lower()
+    re.sub(r'http\S+', '', text)
+
+    text_tokens = word_tokenize(text)
+    all_stopwords = stopwords.words('english')
+    more_stop=["https","tco","via","s","rt","st","w","im","re","m","d","v","a","b","c","e","f",
+               "g","h","lol","l","n","o","p","k","q","r","s","t","u","v","w","ll","ve","tco"]
+    all_stopwords.extend(more_stop)
+    tokens_without_sw = [word for word in text_tokens if not word in all_stopwords]
+    text = (" ").join(tokens_without_sw)
+
     text = re.sub('#><=', '', text)
     text = re.sub('[0-9]+', '', text)
     text = re.sub('[\,]', '', text)
     text = re.sub('[\#]', '', text)
     text = re.sub('[\'\"]', '', text)
     text = re.sub('[\.\$\@\!\*\&\?\_\__]', '', text)
+
+
     '''stop_words =["'tis", "tis", "a", "amp", "30th", "22", "ye", "I", "you", "20th", "me", "get", "st",
                     "sráid", "ll", "le", "tú", "bhí", "faithfully", "tír","https","lol","im"]
     for a in stop_words:
@@ -76,7 +94,7 @@ if __name__ == '__main__':
     y_data=raw_data['target'].copy()
     del raw_data['target']
     X_train, X_val, y_train, y_val = train_test_split(raw_data, y_data, train_size=0.75, random_state=123)
-    cv = CountVectorizer(stop_words="english",min_df=3)
+    cv = CountVectorizer(stop_words="english",min_df=4)
     X = cv.fit_transform(X_train.text_clean)
     column_name=cv.get_feature_names_out()
     X_tr_vec=pd.DataFrame(X.toarray(),columns=column_name)
@@ -102,7 +120,7 @@ if __name__ == '__main__':
 
         print(f"LR. C= {c}.\tTrain ACC: {tr_acc}\tVal Acc: {val_acc}")
     #*************************************** SVM Method *************************************************
-    clf = svm.SVC(kernel='rbf')
+    clf = svm.SVC(kernel='rbf',C=1)
     clf.fit(X_tr_vec, y_train)
     y_train_pred_lr = clf.predict(X_tr_vec)
     y_val_pred_lr = clf.predict(X_val_vec)
@@ -110,6 +128,22 @@ if __name__ == '__main__':
     val_acc = accuracy_score(y_val, y_val_pred_lr)
     print(f"SVM method \tTrain ACC: {tr_acc}\tVal Acc: {val_acc}")
 
+    #**************************************************Gridcv for SVM*************************************
+    '''lr = svm.SVC(kernel='rbf')
+    cv = CountVectorizer(stop_words="english", min_df=3)
+    X = cv.fit_transform(raw_data.text_clean)
+    column_name = cv.get_feature_names_out()
+    X_all = pd.DataFrame(X.toarray(), columns=column_name)
+    parameters = {'kernel': ('linear', 'rbf'), 'C': [0.1,1, 10],'gamma': [0.1,1, 10]}
+    clf = GridSearchCV(lr, parameters,scoring = "accuracy",cv = 4)
+    clf.fit(X_all, y_data)
+    print("best Parameter is: {}".format(clf.best_params_))
+    print("best score is: {}".format(clf.best_score_))'''
+    '''y_train_pred_lr = clf.predict(X_tr_vec)
+    y_val_pred_lr = clf.predict(X_val_vec)
+    tr_acc = accuracy_score(y_train, y_train_pred_lr)
+    val_acc = accuracy_score(y_val, y_val_pred_lr)
+    print(f"SVM method \tTrain ACC: {tr_acc}\tVal Acc: {val_acc}")'''
     #************************************************** train data ******************************************************************
     path = "./nlp-getting-started/test.csv"
     test_data = pd.read_csv(path)
